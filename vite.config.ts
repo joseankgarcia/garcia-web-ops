ff5cd680c9ba2b287d164a32cb92d874f63c07bc
Josean <75704987+joseankgarcia@users.noreply.github.com>
2026-04-25 17:35:34 -0400

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
-  return {
+  base: '/garcia-web-ops/',
+    
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
